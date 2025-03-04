8e7ca762d1705d662070e56736ef45fe55f3cd63
user150434 <o.boudaati@gmail.com>
2025-03-04 15:18:19 +0100

    les 1

---
+++ sketch_mar04a/sketch_mar04a.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // Set pin D3 on the NodeMCU as an
+  // output pin so it can send voltages
+  pinMode(D3, OUTPUT);
+}
+
+void loop() {
+  // set a voltage on D3
+  //digitalWrite(D3, HIGH);
+  // wait a second
+ //// delay(1000);
+  // remove voltage from D3
+  //digitalWrite(D3, LOW);
+  // wait 2 seconds
+  //delay(200);
+
+
+    for (int i = 0; i <= 255; i += 10) { // Verhoog helderheid
+    analogWrite(D3, i);
+    delay(50);
+  }
+  
+  // LED dimmen
+  for (int i = 255; i >= 0; i -= 20) { // Verlaag helderheid
+    analogWrite(D3, i);
+    delay(50);
+  }
+
+
+
+}
